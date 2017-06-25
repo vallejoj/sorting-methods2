f519322745e534cb0e152e15f421ae05a4f4a9b9
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-24 19:14:45 -1000

    Insertion done

---
+++ insertion.js
@@ -0,0 +1,15 @@
+var sort = [233, 4, 1, 33,5,8,99,88, 11, 7, 9,66]
+
+function insert(num){
+  for (var i = 0; i < num.length; i++) {
+    var temp= num[i];
+    var j = i - 1;
+    while(j>=0 && num[j]>temp){
+      num[j + 1] = num[j]
+      j--
+    }
+num[j + 1] = temp;
+  }
+  return num
+}
+console.log(insert(sort))
