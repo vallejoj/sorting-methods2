fa45ffbf4f7f7115299e03e5f423cbcfb53debbe
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-24 18:25:48 -1000

    Got one equation done

--- quick-sort.js
+++ quick-sort.js
@@ -1,24 +1,27 @@
-var sort = [4, 233, 1, 33,5]
+var sort = [33,4,55, 233, 1,5]
 
 function quickSort(number){
-    var pivot= [number[0]]
-var left=[]
-var right=[]
+  var pivot= [number[0]]
+  var left=[]
+  var right=[]
+
+  if(number.length<1){
+    return number
+  }
 for (var i = 1; i <number.length; i++) {
-      if(number.length>1){
-        if (pivot>number[i]) {
+        if (pivot>=number[i]) {
           left.push(number[i])
         }else{
           right.push(number[i])
         }
-      }
+
     }
 
-    
+
     console.log(right)
     console.log(left)
     console.log(pivot)
-  return number
+  return quickSort(left).concat(pivot,quickSort(right))
 
 }
 
