25920118fdbee65124157a1f317b50c6d99a6b9d
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-24 16:59:41 -1000

    Got my left and right array to work

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,25 @@
-function methodName (arguments) {
-  // body...
+var sort = [4, 233, 1, 33,5]
+
+function quickSort(number){
+    var pivot= [number[0]]
+var left=[]
+var right=[]
+for (var i = 1; i <number.length; i++) {
+      if(number.length>1){
+        if (pivot>number[i]) {
+          left.push(number[i])
+        }else{
+          right.push(number[i])
+        }
+      }
+    }
+
+    
+    console.log(right)
+    console.log(left)
+    console.log(pivot)
+  return number
+
 }
+
+console.log(quickSort(sort))
