34c6c68ab000f870aafb201f4ad2f4ec5e911e04
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-28 07:51:06 -1000

    Creating read me and finished insertion

--- insertion.js
+++ insertion.js
@@ -2,14 +2,17 @@ var sort = [233, 4, 1, 33,5,8,99,88, 11, 7, 9,66]
 
 function insert(num){
   for (var i = 0; i < num.length; i++) {
+
     var temp= num[i];
     var j = i - 1;
     while(j>=0 && num[j]>temp){
+      console.log(i,j)         
       num[j + 1] = num[j]
       j--
     }
-num[j + 1] = temp;
+     num[j + 1] = temp;
   }
+
   return num
 }
 console.log(insert(sort))
